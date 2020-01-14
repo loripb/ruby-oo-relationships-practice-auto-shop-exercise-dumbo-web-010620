ad5c908d15b15bda776ccaafade60332b0238794
Lori Boyd <lori.p.boyd@gmail.com>
2020-01-14 16:57:54 +0000

    Automatically backed up by Learn

--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,24 @@ def reload
   load 'config/environment.rb'
 end
 
+# Mechanics
+joe = Mechanic.new('Joe', 'antique')
+kay = Mechanic.new('Kay', 'exotic')
+mike = Mechanic.new('Mike', 'clunker')
+cindy = Mechanic.new('Cindy', 'exotic')
+
+#owners
+owner1 = CarOwner.new('Lei')
+owner2 = CarOwner.new('Tay')
+
+# cars
+car1 = Car.new('Chevey', 'Impala', joe.specialty, owner2)
+car2 = Car.new('lambo', 'sport', cindy.specialty, owner1)
+car3 = Car.new('volts waggon', 'bus', mike.specialty, owner2)
+car4 = Car.new('volts waggon', 'buggie', mike.specialty, owner1)
+
+
+
+
 binding.pry
+0
